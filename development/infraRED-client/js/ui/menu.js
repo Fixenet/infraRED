2d067fb54577c189ad8c1ebe29db289dabab4c05
Fixenet <fixenet5@gmail.com>
2022-04-01 20:09:57 +0100

    Added buttons to log the node lists

--- development/infraRED-client/js/ui/menu.js
+++ development/infraRED-client/js/ui/menu.js
@@ -13,6 +13,33 @@ infraRED.editor.menu = (function() {
             title.innerHTML = "Menu";
         
             menuBar.append(title);
+
+            let content = document.createElement("div");
+            content.className = "content";
+
+            menuBar.append(content);
+
+            let logCanvasButton = document.createElement("button");
+            let logResourcesButton = document.createElement("button");
+
+            logCanvasButton.className = "menu-button";
+            logCanvasButton.id = "log-canvas-button";
+            logCanvasButton.innerHTML = "Log Canvas Nodes";
+
+            $(logCanvasButton).on("click", () => {
+                infraRED.events.emit("nodes:log-canvas");
+            });
+
+            logResourcesButton.className = "menu-button";
+            logResourcesButton.id = "log-resources-button";
+            logResourcesButton.innerHTML = "Log Resource Nodes";
+
+            $(logResourcesButton).on("click", () => {
+                infraRED.events.emit("nodes:log-resources");
+            });
+
+            content.append(logResourcesButton);
+            content.append(logCanvasButton);
         },
         get: function() {
             return menuBar;
