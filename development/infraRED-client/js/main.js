69fc0838fcd02958026864dfaebe290ee3dd4733
Fixenet <fixenet5@gmail.com>
2022-08-12 16:38:39 +0100

    Disabled the window unload, placed events debug

--- development/infraRED-client/js/main.js
+++ development/infraRED-client/js/main.js
@@ -1,8 +1,9 @@
 //'backend' for client side
 infraRED.init();
+infraRED.events.DEBUG = true; //prints logs
 //frontend/views for client side
 infraRED.editor.init();
 
 //ask the user if they really want to leave
 //in case we implement some saving functionality
-window.onbeforeunload = function() { return true; };
+//TODO - window.onbeforeunload = function() { return true; };
