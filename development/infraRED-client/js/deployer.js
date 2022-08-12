277d615a2d1e4b6b62ca988c3f0f92f26f7579e7
Fixenet <fixenet5@gmail.com>
2022-08-12 16:43:06 +0100

    Moved deploy method to the deployer section

--- development/infraRED-client/js/deployer.js
+++ development/infraRED-client/js/deployer.js
@@ -1,7 +1,45 @@
 infraRED.deployer = (function () {
+    function deployNodes() {
+        //TODO - deepcopy of my node obj list, can probably be done better
+        // i can also use this to only pass up to the server the values i want (reduce clutter)
+        let cleanNodeList = [];
+        for (let node of infraRED.nodes.canvasList.getAll()) {
+            let cleanNode = jQuery.extend(true, {}, node);
+
+            let cleanList = [];
+            for (let relationship of cleanNode.relationships) {
+                let clean = jQuery.extend(true, {}, relationship);
+                delete clean.lineSVG;
+                delete clean.lineOffsetPlot;
+                cleanList.push(clean);
+            }
+
+            cleanNode.relationships = cleanList;
+            cleanNodeList.push(cleanNode);
+        }
+
+        // post the current nodes in use to server to start deployment
+        $.ajax({
+            method: 'POST', url: '/deploy',
+            contentType: 'application/json', dataType: 'json',
+            async: false,
+
+            data: JSON.stringify({
+                "nodes": cleanNodeList
+            }),
+
+            //TODO - success function places value inside the return variable
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    }
+
     return {
         init: function() {
             console.log('Starting the deployment functionality.');
+
+            infraRED.events.on('relationships:deploy', deployNodes);
         }
     };
 })();
