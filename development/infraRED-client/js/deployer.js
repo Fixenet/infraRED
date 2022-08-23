013a97f6b0ded989e34cd5b13e6130ec3f369b69
Fixenet <fixenet5@gmail.com>
2022-08-23 13:43:16 +0100

    Added save func and fixed deep copy redundancy.

--- development/infraRED-client/js/deployer.js
+++ development/infraRED-client/js/deployer.js
@@ -1,23 +1,22 @@
 infraRED.deployer = (function () {
-    function deployNodes() {
+    function cleanUpCanvasList() {
         //TODO - deepcopy of my node obj list, can probably be done better
         //i can also use this to only pass up to the server the values i want (reduce clutter)
         let cleanNodeList = [];
         for (let node of infraRED.nodes.canvasList.getAll()) {
-            let cleanNode = jQuery.extend(true, {}, node);
-            
-            let cleanList = [];
+            let cleanNode = $.extend(true, {}, node);
+        
             for (let relationship of cleanNode.relationships) {
-                let clean = jQuery.extend(true, {}, relationship);
-                delete clean.lineSVG;
-                delete clean.lineOffsetPlot;
-                cleanList.push(clean);
+                delete relationship.lineSVG;
+                delete relationship.lineOffsetPlot;
             }
 
-            cleanNode.relationships = cleanList;
             cleanNodeList.push(cleanNode);
         }
+        return cleanNodeList;
+    }
 
+    function deployNodes() {
         //post the current nodes in use to server to start deployment
         $.ajax({
             method: 'POST', url: '/deploy',
@@ -25,7 +24,26 @@ infraRED.deployer = (function () {
             async: false,
 
             data: JSON.stringify({
-                "nodes": cleanNodeList
+                "nodes": cleanUpCanvasList()
+            }),
+
+            //TEST - we get the data back from the server
+            success: function(data) {
+                console.log(data);
+            }
+        });
+    }
+
+    function saveNodes(name) {
+        //post the current nodes in use to server to start deployment
+        $.ajax({
+            method: 'POST', url: '/save',
+            contentType: 'application/json', dataType: 'json',
+            async: false,
+
+            data: JSON.stringify({
+                "name": name,
+                "nodes": cleanUpCanvasList()
             }),
 
             //TEST - we get the data back from the server
@@ -40,6 +58,7 @@ infraRED.deployer = (function () {
             console.log('Starting the deployment functionality.');
 
             infraRED.events.on('relationships:deploy', deployNodes);
+            infraRED.events.on('relationships:save', saveNodes);
         }
     };
 })();
